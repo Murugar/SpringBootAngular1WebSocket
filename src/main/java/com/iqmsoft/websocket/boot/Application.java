2537b714ef151d40970b85ccbdd5797e4eb693e7
Davanon <davanon2014@gmail.com>
2017-03-30 19:36:31 +0530

    Second

--- src/main/java/com/iqmsoft/websocket/boot/Application.java
+++ src/main/java/com/iqmsoft/websocket/boot/Application.java
@@ -2,8 +2,11 @@ package com.iqmsoft.websocket.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
 
 @Configuration
 @ComponentScan
@@ -12,4 +15,6 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    
+  
 }
